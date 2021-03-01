c70e326c9569780f6b5bf55e71401c738e12b68d
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 16:30:59 -0400

    Add files via upload
    Un programa que te pida tu nombre y lo muestre en pantalla separando cada letra de la siguiente con un espacio. Por ejemplo, si tu nombre es "Juan", debería aparecer en pantalla "J u a n".

---
+++ Pracita_separar_nombre_con_espacios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pracita_separar_nombre_con_espacios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte un nombre:");
+            string nombre = Console.ReadLine();            
+            string valor = "";
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                char letra = nombre[i];
+                valor +=letra+ " ";
+            }
+            Console.WriteLine(valor);
+        }
+    }
+}
